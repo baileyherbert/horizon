24ddad70c515c1a9f52171818e6feed0d79fc50a
Bailey Herbert <bailey.herbert@hotmail.com>
2018-09-22 23:35:12 -0700

    Port all Horizon tags into Twig functions

--- horizon/lib/View/Extensions/HorizonExtension.php
+++ horizon/lib/View/Extensions/HorizonExtension.php
@@ -6,8 +6,13 @@ use Horizon\Framework\Kernel;
 
 use Twig_SimpleFunction;
 use Horizon\Utils\TimeProfiler;
+use Horizon\View\ViewExtension;
+use Horizon\Routing\RouteParameterBinder;
+use Horizon\Routing\RouteLoader;
+use Horizon\Http\MiniRequest;
+use Horizon\Utils\Path;
 
-class HorizonExtension extends \Twig_Extension implements \Twig_Extension_GlobalsInterface
+class HorizonExtension extends ViewExtension
 {
 
     public function getGlobals()
@@ -20,23 +25,64 @@ class HorizonExtension extends \Twig_Extension implements \Twig_Extension_Global
         );
     }
 
-    public function getFunctions()
+    public function getTranspilers()
     {
         return array(
-            $this->csrf(),
-            $this->lang(),
-            $this->runtime()
+            'csrf' => 'csrf',
+            '__' => '__',
+            'local' => '__',
+            'localize' => '__',
+            'runtime' => 'runtime',
+            'link' => 'link',
+            'public' => 'public',
+            'image' => 'image',
+            'file' => 'file',
+            'script' => 'script',
+            'style' => 'style',
+            'include' => function($template) { return "{% include $template %}"; },
+            'extend' => function($template) { return "{% extends $template %}"; },
+            'section' => function($template) {
+                $name = trim($template, chr(34) . chr(39));
+
+                return "{% block $name %}";
+            },
+            'endsection' => function($template) { return "{% endblock %}"; }
         );
     }
 
-    protected function csrf()
+    protected function getPublicAssetPath($relativePath, $scope = null)
+    {
+        $request = Kernel::getRequest();
+        $currentPath = $request->path();
+        $root = rtrim(Path::getRelative($currentPath, '/', $_SERVER['SUBDIRECTORY']), '/');
+
+        $fromExtension = ($scope === 'ext' || $scope === 'extension' || $scope === 'e');
+
+        if ($fromExtension) {
+            if (!is_null($this->getExtension())) {
+                $relative = ltrim($relativePath, '/');
+                $publicPathLegacy = sprintf('%s/%s', $root, ltrim($this->getExtension()->getMappedLegacyRoute($relative), '/'));
+                $publicPathRouted = sprintf('%s/%s', $root, ltrim($this->getExtension()->getMappedPublicRoute($relative), '/'));
+
+                return (USE_LEGACY_ROUTING) ? $publicPathLegacy : $publicPathRouted;
+            }
+        }
+
+        if (USE_LEGACY_ROUTING) {
+            return $root . '/app/public/' . ltrim($relativePath, '/');
+        }
+
+        return $root . '/' . ltrim($relativePath, '/');
+    }
+
+    protected function twigCsrf()
     {
         return new Twig_SimpleFunction('csrf', function () {
-            return 'boo';
+            return '<input type="hidden" name="csrf" value="{{ csrf() | e(\'html_attr\') }}">';
         });
     }
 
-    protected function lang()
+    protected function twigLang()
     {
         return new Twig_SimpleFunction('__', function ($context, $text) {
             $bucket = Kernel::getLanguageBucket();
@@ -44,10 +90,63 @@ class HorizonExtension extends \Twig_Extension implements \Twig_Extension_Global
         }, array('needs_context' => true));
     }
 
-    protected function runtime()
+    protected function twigRuntime()
     {
         return new Twig_SimpleFunction('runtime', function () {
             return TimeProfiler::time('kernel');
+        });
+    }
+
+    protected function twigLink()
+    {
+        return new Twig_SimpleFunction('link', function ($toPath) {
+            $request = Kernel::getRequest();
+            return $request->getLinkTo($toPath);
+        });
+    }
+
+    protected function twigPublic()
+    {
+        $handler = $this;
+
+        return new Twig_SimpleFunction('public', function ($relativePath, $scope = null) use ($handler) {
+            return $handler->getPublicAssetPath($relativePath, $scope);
+        });
+    }
+
+    protected function twigImage()
+    {
+        $handler = $this;
+
+        return new Twig_SimpleFunction('image', function ($relativePath, $scope = null) use ($handler) {
+            return $handler->getPublicAssetPath('/images/' . ltrim($relativePath, '/'), $scope);
+        });
+    }
+
+    protected function twigFile()
+    {
+        $handler = $this;
+
+        return new Twig_SimpleFunction('file', function ($relativePath, $scope = null) use ($handler) {
+            return $handler->getPublicAssetPath('/files/' . ltrim($relativePath, '/'), $scope);
+        });
+    }
+
+    protected function twigScript()
+    {
+        $handler = $this;
+
+        return new Twig_SimpleFunction('script', function ($relativePath, $scope = null) use ($handler) {
+            return $handler->getPublicAssetPath('/scripts/' . ltrim($relativePath, '/'), $scope);
+        });
+    }
+
+    protected function twigStyle()
+    {
+        $handler = $this;
+
+        return new Twig_SimpleFunction('style', function ($relativePath, $scope = null) use ($handler) {
+            return $handler->getPublicAssetPath('/styles/' . ltrim($relativePath, '/'), $scope);
         });
     }
 
