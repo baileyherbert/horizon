15fa5d02d6278728ce7104d4ffe2ed1b4d145fee
Bailey Herbert <ballenherbert@gmail.com>
2018-11-16 19:32:53 -0700

    Add shortcuts for getting a middleware's request and response instances

--- horizon/lib/Routing/Middleware.php
+++ horizon/lib/Routing/Middleware.php
@@ -10,7 +10,27 @@ class Middleware
 
     public function __invoke(Request $request, Response $response)
     {
-        
+
+    }
+
+    /**
+     * Gets the Request instance for this middleware.
+     *
+     * @return Request
+     */
+    public function getRequest()
+    {
+        return Kernel::getRequest();
+    }
+
+    /**
+     * Gets the Response instance for this middleware.
+     *
+     * @return Response
+     */
+    public function getResponse()
+    {
+        return Kernel::getResponse();
     }
 
 }
