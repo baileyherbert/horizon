41fa60e0c875b36cdcecf0c638e98aafb41b6ecd
Bailey Herbert <bailey.herbert@hotmail.com>
2019-09-03 04:55:48 -0700

    Allow dynamic component to extend an object

--- horizon/lib/View/Component/DynamicComponent.php
+++ horizon/lib/View/Component/DynamicComponent.php
@@ -6,22 +6,60 @@ use Horizon\Support\Facades\Component;
 
 class DynamicComponent extends Component {
 
-    protected $options = array();
+    protected $target = array();
 
-    public function __construct($options = array()) {
-        $this->options = $options;
+    public function __construct($target = array()) {
+        $this->target = $target;
     }
 
     public function __isset($name) {
-        return array_key_exists($name, $this->options);
+        if (is_array($this->target)) {
+            return array_key_exists($name, $this->target);
+        }
+
+        if (is_object($this->target)) {
+            return property_exists($this->target, $name);
+        }
+
+        return false;
     }
 
     public function __get($name) {
-        return array_get($this->options, $name);
+        if (is_array($this->target)) {
+            return array_get($this->target, $name);
+        }
+
+        if (is_object($this->target)) {
+            return $this->target[$name];
+        }
+
+        if (is_string($name) && is_int($name)) {
+            return $this->target[$name];
+        }
+    }
+
+    public function __call($name, $args) {
+        if (is_object($this->target)) {
+            if (is_callable(array($this->target, $name))) {
+                return call_user_func_array(array($this->target, $name), $args);
+            }
+        }
     }
 
     public function __set($name, $value) {
-        return array_set($this->options, $name, $value);
+        if (is_array($this->target)) {
+            array_set($this->target, $name, $value);
+            return;
+        }
+
+        if (is_object($this->target)) {
+            $this->target[$name] = $value;
+            return;
+        }
+    }
+
+    public function __toString() {
+        if (is_string($this->target)) return $this->target;
     }
 
 }
