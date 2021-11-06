a9ece1d317e507fcd5aa71f7454beaa26eeb3421
Bailey Herbert <hello@bailey.sh>
2021-11-06 05:56:21 -0700

    Make encryption more secure
    * Added an APP_SECRET variable for the encryption key
    * Made the initialization vector random

--- horizon/lib/Encryption/FastEncrypt.php
+++ horizon/lib/Encryption/FastEncrypt.php
@@ -2,7 +2,9 @@
 
 namespace Horizon\Encryption;
 
+use Exception;
 use phpseclib\Crypt\AES;
+use phpseclib\Crypt\Random;
 
 /**
  * A lightweight and fast symmetric key encryption with automatic key construction. This is meant for use in storage
@@ -15,16 +17,21 @@ class FastEncrypt {
 	 * @param string $data The text to encrypt.
 	 */
 	public static function encrypt($data) {
-		$cipher = self::generateCipher();
-		return $cipher->encrypt($data);
+		$iv = self::generateIV();
+		$cipher = self::generateCipher($iv);
+		$data = $cipher->encrypt($data);
+
+		return $iv . $data;
 	}
 
 	/**
 	 * @param string $encrypted The encrypted data to decrypt.
 	 */
 	public static function decrypt($encrypted) {
-		$cipher = self::generateCipher();
-		return $cipher->decrypt($encrypted);
+		list($iv, $data) = self::extractIV($encrypted);
+
+		$cipher = self::generateCipher($iv);
+		return $cipher->decrypt($data);
 	}
 
 	/**
@@ -32,23 +39,27 @@ class FastEncrypt {
 	 *
 	 * @return AES
 	 */
-	private static function generateCipher() {
+	private static function generateCipher($iv) {
 		$cipher = new AES();
 		$blockLength = ($cipher->getBlockLength() >> 3);
 
 		$cipher->setKey($blockLength, self::generateKey());
-		$cipher->setIV($blockLength, self::generateIV());
+		$cipher->setIV($blockLength, $iv);
 
 		return $cipher;
 	}
 
 	/**
-	 * Generates a relatively secure key based on the server's configuration and the framework's environment.
+	 * Generates a key to use for encryption.
 	 *
 	 * @param $length
 	 * @return string Encryption key containing $length number of characters.
 	 */
 	private static function generateKey($length = 32) {
+		if (env('APP_SECRET')) {
+			return substr(hash('sha256', sprintf('FE:%s', env('APP_SECRET'))), 0, $length);
+		}
+
 		$base = substr(md5(__DIR__), 0, 10);
 
 		if (isset($_SERVER['PATH'])) $base .= substr(md5($_SERVER['PATH']), 2, 8);
@@ -66,23 +77,31 @@ class FastEncrypt {
 	}
 
 	/**
-	 * Generates an IV based on the server's configuration and the framework's environment.
+	 * Generates a random IV for encryption.
 	 *
 	 * @param $length
 	 * @return string Encryption IV containing $length number of characters.
 	 */
 	private static function generateIV($length = 32) {
-		$base = substr(md5(__DIR__), 0, 5);
+		return Random::string($length);
+	}
 
-		if (@ini_get('extension_dir')) $base .= substr(md5(ini_get('extension_dir')), 2, 4);
-		if (@ini_get('error_reporting')) $base .= substr(md5(ini_get('error_reporting')), 2, 4);
-		if (@ini_get('realpath_cache_size')) $base .= substr(md5(ini_get('realpath_cache_size')), 2, 4);
-		if (function_exists('curl_version')) $base .= substr(md5(json_encode(curl_version())), 2, 4);
+	/**
+	 * Extracts the data and IV from a cipher.
+	 *
+	 * @param string $data
+	 * @param int $length
+	 * @return string[]
+	 */
+	private static function extractIV($data, $length = 32) {
+		if (strlen($data) <= $length) {
+			throw new Exception('Encrypted data is too short and may be lacking an initialization vector');
+		}
 
-		$first = substr(md5(substr($base, 0, (strlen($base) / 2))), 0, floor($length / 2));
-		$last = substr(md5(substr($base, (strlen($base) / 2))), 0, ceil($length / 2));
+		$iv = substr($data, 0, $length);
+		$data = substr($data, $length);
 
-		return $first . $last;
+		return [$iv, $data];
 	}
 
 }
