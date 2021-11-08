8b37e6771bbec71dcf6bcb4b2ab82f57deb464aa
Bailey Herbert <hello@bailey.sh>
2021-11-08 06:13:34 -0700

    Prevent console invocation from the web server

--- horizon/bootstrap/console.php
+++ horizon/bootstrap/console.php
@@ -1,5 +1,10 @@
 <?php
 
+if (isset($_SERVER['REMOTE_ADDR'])) {
+	echo "Error: Unsupported environment";
+	exit(1);
+}
+
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 Horizon\Foundation\Services\Environment::set('HORIZON_MODE', 'console');
