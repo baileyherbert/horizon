24e98d75c24a3732eede73e22e1105c4ea4e1fc7
Bailey Herbert <hello@bailey.sh>
2022-02-10 21:26:45 -0700

    Add replacement options to the SPA controller

--- src/Routing/Controllers/SinglePageActionController.php
+++ src/Routing/Controllers/SinglePageActionController.php
@@ -5,11 +5,24 @@ namespace Horizon\Routing\Controllers;
 use Horizon\Foundation\Application;
 use Horizon\Http\Response;
 use Horizon\Http\Controller;
+use Horizon\Http\Request;
 
 class SinglePageActionController extends Controller {
 
-	public function __invoke(Response $response, $spaFilePath) {
-		$response->sendFile(Application::root($spaFilePath));
+	public function __invoke(Request $request, Response $response, $spaFilePath) {
+		$value = file_get_contents(Application::root($spaFilePath));
+		$plainRules = $request->route()->getOption('rewrite', []);
+		$regexRules = $request->route()->getOption('rewrite.regex', []);
+
+		foreach ($plainRules as $rule => $replacement) {
+			$value = str_replace($rule, $replacement, $value);
+		}
+
+		foreach ($regexRules as $expression => $replacement) {
+			$value = preg_replace($expression, $replacement, $value);
+		}
+
+		$response->write($value);
 	}
 
 }
